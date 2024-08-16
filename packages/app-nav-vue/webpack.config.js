096bea12e1b388f888c99494435035e50e236ad3
Nicky Wang <shuowang823@gmail.com>
2024-08-16 14:46:35 +0800

    fix: use webpack config to deal with outdated config issues

---
+++ packages/app-nav-vue/webpack.config.js
@@ -0,0 +1,89 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('node:path')
+const { VueLoaderPlugin } = require('vue-loader')
+const StandaloneSingleSpaPlugin = require('standalone-single-spa-webpack-plugin')
+
+const isProd = process.env.NODE_ENV === 'production' // 是否生产环境
+
+function resolve(dir) {
+  return path.resolve(__dirname, dir)
+}
+
+const { name } = require('./package.json')
+
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+
+  entry: {
+    // use .js instead of .ts, because the types of single-spa-vue is outdated
+    app: './src/main.js'
+  },
+
+  output: {
+    // path: resolve('dist'),
+    // filename: '[name].js'
+    libraryTarget: 'system',
+    devtoolNamespace: name
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: [{ loader: 'vue-loader' }, { loader: 'vue-svg-inline-loader' }],
+        include: [resolve('src')],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+        include: [resolve('src')]
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        },
+        include: [resolve('src')],
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin({}),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      favicon: './public/favicon.ico'
+    }),
+    new StandaloneSingleSpaPlugin({
+      appOrParcelName: name,
+      disabled: process.env.STANDALONE_SINGLE_SPA !== 'true'
+    })
+  ],
+
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src') // Ensure this matches your setup
+    }
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    port: 9020,
+    hot: true,
+    allowedHosts: 'all',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }
+}
