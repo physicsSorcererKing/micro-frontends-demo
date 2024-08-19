84c36f5b1964655819d8983db13edd6b40c9bd4a
Nicky Wang <shuowang823@gmail.com>
2024-08-19 14:42:06 +0800

    chore: use standalone component instead module for angular 17+ recommendation

--- packages/app-main-angular/src/main.single-spa.ts
+++ packages/app-main-angular/src/main.single-spa.ts
@@ -1,23 +1,33 @@
 import { enableProdMode, NgZone } from '@angular/core';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Router, NavigationStart } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { Router, NavigationStart, provideRouter } from '@angular/router';
 
-import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
+import {
+  singleSpaAngular,
+  getSingleSpaExtraProviders,
+} from 'single-spa-angular';
 
-
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+import { APP_BASE_HREF } from '@angular/common';
+import { EmptyRouteComponent } from './app/empty-route/empty-route.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
 const lifecycles = singleSpaAngular({
-  bootstrapFunction: singleSpaProps => {
+  bootstrapFunction: (singleSpaProps) => {
     singleSpaPropsSubject.next(singleSpaProps);
-    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+    return bootstrapApplication(AppComponent, {
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' },
+        getSingleSpaExtraProviders(),
+        provideRouter([{ path: '**', component: EmptyRouteComponent }]),
+      ],
+    });
   },
   template: '<app-root />',
   Router,
