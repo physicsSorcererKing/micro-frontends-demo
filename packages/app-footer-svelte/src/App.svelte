adf7cce8985db5b0a94ed2fff93ba7dc365fae48
Nicky Wang <shuowang823@gmail.com>
2024-09-10 19:40:12 +0800

    feat: add svelte footer layout

--- packages/app-footer-svelte/src/App.svelte
+++ packages/app-footer-svelte/src/App.svelte
@@ -1,11 +1,25 @@
 <script>
+
+  import { IconBrandSvelte } from '@tabler/icons-svelte';
   export let name;
 </script>
 
-<section>{name} is mounted!</section>
+<section>
+  <IconBrandSvelte size="36" />
+  <p>Svelte Footer</p>
+</section>
 
 <style>
   section {
+    background: #4A525A;
+    color: white;
+    display: flex;
+    height: 52px;
+    padding: 0 8px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
     font-size: 1.5rem;
+    font-weight: 600;
   }
 </style>
